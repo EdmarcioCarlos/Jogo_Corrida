f495ba6124b71c063ecdd9a6cb3abe870bc3b02c
Edmarcio Carlos Fonseca Junior <Edmarcio.junior@outlook.com>
2023-12-01 11:20:49 -0300

    Tela de Load (ok)  Cenario 01 ( a terminar )
    Tela de load funcional ainda falta a criação da pista 01

--- Jogo_Corrida/Assets/Scripts/Menu_iNICIAL/Controlador_MenuInicial.cs
+++ Jogo_Corrida/Assets/Scripts/Menu_iNICIAL/Controlador_MenuInicial.cs
@@ -1,20 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Controlador_MenuInicial : MonoBehaviour
 {
     [Tooltip("Anexar os menus inicias do jogo para funcionamento da scene Menu_Inicial")]
-    [SerializeField] private GameObject Menu_Inicial,
-                                        Menu_Config;
+    [SerializeField] private GameObject Menu_Inicial;
+    [SerializeField] private GameObject Menu_Config;
+    [SerializeField] private GameObject Controlador_TelaLoad;
 
-    [SerializeField] private GameObject Controlador_TelaLoade;
+
+
+
+    [Tooltip("Nome da fase que deve ser iniciar atualmente")]
+    [SerializeField] private string Nome_Fase;
+
+
+    [Header("Barra de Load")]
+    [SerializeField] private Slider Barra_Load;
+    [SerializeField] private TextMeshProUGUI Texto_Progressão ;
+
     public void Awake()
     {
         Menu_Inicial.SetActive(true);
         Menu_Config.SetActive(false);
+        Controlador_TelaLoad.SetActive(false);
     }
     public void Ativar_Config()
     {
@@ -30,11 +45,32 @@ public class Controlador_MenuInicial : MonoBehaviour
 
     public void Iniciar()
     {
-        Controlador_TelaLoade.GetComponent<Load_Scene>().Carregar_Scene();
+        Controlador_TelaLoad.SetActive(true);
+        Carregar_Scene();
+
+
     }
     public void Sair_Jogo()
     {
         Debug.Log("Saindo");
         Application.Quit();
+    }
+
+    public void Carregar_Scene()
+    {
+        StartCoroutine(Tela_Load(Nome_Fase));
+    }
+
+    IEnumerator Tela_Load(string _Nome_Fase)
+    {
+        AsyncOperation OperadorLoad = SceneManager.LoadSceneAsync(_Nome_Fase);
+
+        while(!OperadorLoad.isDone)
+        {
+            float Progressão = Mathf.Clamp01(OperadorLoad.progress/0.1f);
+            Texto_Progressão.text = Progressão + "%"; 
+            Barra_Load.value = Progressão;
+            yield return null;
+        }
     }
 }
